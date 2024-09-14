1ba4cba4553a2150eec8d688f097f47025f3cb68
PriyankaJonnnala <144570902+PriyankaJonnnala@users.noreply.github.com>
2024-09-14 15:33:22 -0400

    Update sqlite-example.py

--- sqlite-example.py
+++ sqlite-example.py
@@ -6,14 +6,10 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade
 REAL)''')
 cursor.execute("INSERT INTO students (name, grade) VALUES ('Alice', 85.5)")
+cursor.execute("INSERT INTO students (name, grade) VALUES ('Bob', 92.3)")
 cursor.execute("SELECT * FROM students")
 rows = cursor.fetchall()
 for row in rows:
     print(row)
-try:
-   cursor.execute("SELECT * FROM non_existing_table")
-except sqlite3.OperationalError as e:
-  print(f"An error occurred: {e}")
-cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ('Bob', 92.3))
 connection.commit()
 connection.close()
